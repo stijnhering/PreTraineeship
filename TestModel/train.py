79e4e69379d580f934bf9209643635996d45ad57
melchiorhering <stijn@hering.nl>
2022-03-30 14:10:40 +0200

    Update

---
+++ TestModel/train.py
@@ -0,0 +1,15 @@
+import os
+import warnings
+import sys
+
+import pandas as pd
+import numpy as np
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import ElasticNet
+from urllib.parse import urlparse
+import mlflow
+import mlflow.sklearn
+import dvc.api
+
+import logging
